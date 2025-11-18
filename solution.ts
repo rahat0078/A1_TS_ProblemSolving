9954fd4f931693e488812a89f0d9b5db56f2e315
rahat0078 <rarahat1710@gmail.com>
2025-11-18 16:19:39 +0600

    problem 2 solved

--- solution.ts
+++ solution.ts
@@ -1,12 +1,21 @@
-const formatValue = (value: string | number | boolean): string | number | boolean  => {
-    if(typeof value === "string") {
+const formatValue = (value: string | number | boolean): string | number | boolean => {
+    if (typeof value === "string") {
         return value.toUpperCase()
-    } else if(typeof value === "number") {
+    } else if (typeof value === "number") {
         const multiple = value * 10;
         return multiple
-    } else if(typeof value === "boolean") {
+    } else if (typeof value === "boolean") {
         return !value
-    } 
+    }
     return value
 }
 
+const getLength = (input: string | []): number => {
+    if(typeof input === "string") {
+        const totalChar = input.length;
+        return totalChar;
+    } else if (Array.isArray(input)){
+        return input.length
+    } 
+    return input
+}
